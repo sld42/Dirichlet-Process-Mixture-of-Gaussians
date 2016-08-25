2bc8a183b8b6c5c45b1ef418cc4109385eece6bf
Sam Di <samuel.di916@gmail.com>
2016-08-25 04:29:52 -0400

    Update DP_Main.py

--- DP_Main.py
+++ DP_Main.py
@@ -10,7 +10,6 @@ for step in range(0,6):
     cluster=[]
     alpha=np.linspace(np.finfo(float).eps,2,6)
     weights=stickbreak(alpha[3])
-    weights=stickbreak(1.5)
     n=100
     R = np.random.multinomial(n,weights/sum(weights))
     ind=R[np.nonzero(R)]
